347a8ee8061df12267a605b3de2f8ae9fad516ff
jsmith3763 <jsmith3763@gmail.com>
2022-08-04 13:13:12 -0700

    Updated Postitem

--- treasure_bay/src/components/ProductItem/PostItemPage.js
+++ treasure_bay/src/components/ProductItem/PostItemPage.js
@@ -25,130 +25,130 @@ function PostItemPage() {
             console.log(imageSent)
             if (images) {
                   for (let i = 0; i < images.length; i++) {
-                    formData.append("images", images[i]);
+                        formData.append("images", images[i]);
                   }
             }
 
-                  formData.append("productName", productName);
-                  formData.append("price", price);
-                  formData.append("details", details);
-                  formData.append("description", description);
-                  // formData.append("images", images[0]);
-                  formData.append("user_id", user_id);
-                  
+            formData.append("productName", productName);
+            formData.append("price", price);
+            formData.append("details", details);
+            formData.append("description", description);
+            // formData.append("images", images[0]);
+            formData.append("user_id", user_id);
+
             console.log(JSON.stringify(formData))
             console.log(images)
 
-           
-              
-                    
-            
+
+
+
+
             await fetch("http://localhost:3025/postitem", {
                   method: 'POST',
                   body: formData,
             })
-            .then((result) => {
-                  console.log("File sent successfully")
-                  console.log(result)
-            })
-            .catch((err) => {
-                  console.log(err.message)
-            });
-          };
+                  .then((result) => {
+                        console.log("File sent successfully")
+                        console.log(result)
+                  })
+                  .catch((err) => {
+                        console.log(err.message)
+                  });
+      };
 
-          const fileChangeHandler = (e) => {
+      const fileChangeHandler = (e) => {
             setFileData(e.target.files[0]);
-          };
-        
-          const onSubmitHandler = (e) => {
+      };
+
+      const onSubmitHandler = (e) => {
             // e.preventDefault();
-        
+
             const data = new FormData();
-        
+
             data.append("image", fileData);
-        
+
             fetch("http://localhost:3025/images", {
                   method: "POST",
                   body: data,
             })
-              .then((result) => {
-                console.log("File sent successfully");
-              })
-              .catch((err) => {
-                console.log(err.message);
-              });
-          };
-  return (
-       <>
-        <form>
-        <div class="container">
-            <div class="row">
-                <div class="col-25">
-                      <label className="pn" >Product Name</label>
-                </div>
-                <div class="col-75">
-                      <input type="text" value={productName} onChange={(e) => setProductName(e.target.value )} className="pi"
-                      />
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-25">
-                      <label className="pn">Product Price</label>
-                </div>
-                <div class="col-75">
-                      <input type="text" value={price} onChange={(e) => setPrice(e.target.value )} className="pi" />
-                </div>
-            </div>
-            <div class="row">
-                      <div class="col-25">
-                      <label className="pn">Product Details</label>
-            </div>
-            <div class="col-75">
-                      <textarea value={details} onChange={(e) => setDetails(e.target.value )} className="pi" rows="7" cols="50"></textarea>
-            </div>
-            </div>
-            <div class="row">
-            <div class="col-25">
-                  <label className="pn">Product Description</label>
-            </div>
-            <div class="col-75">
-                  <textarea value={description} onChange={(e) => setDescription(e.target.value )} className="pi" rows="7" cols="50"></textarea>
-            </div>
-      </div>
-      <div class="row">
-            <div class="col-25">
-                  <label className="pn" >Product Image</label>
-            </div>
-            <div class="col-75">
-                  <AppDropZone images={images} setImages={setImages} setImageSent={setImageSent}/>
-                  <button className="btn" type="submit" onClick={(e) => {
-                        e.preventDefault();
-                        postItem(productName, price, details, description, imageSent, user_id)
-                        // setFileData(images[0]);
-                        // console.log(images[0])
-                        // onSubmitHandler();
-                  }}>
-                        Submit
-                  </button>
-            </div>
-      </div>
-      <div class="row">
-      
-      
-            {/* <DropZone images={images} setImages={setImages}/> */}
-            
-            {/* <div className="box__input">
+                  .then((result) => {
+                        console.log("File sent successfully");
+                  })
+                  .catch((err) => {
+                        console.log(err.message);
+                  });
+      };
+      return (
+            <>
+                  <form>
+                        <div class="container">
+                              <div class="row">
+                                    <div class="col-25">
+                                          <label className="pn" >Product Name</label>
+                                    </div>
+                                    <div class="col-75">
+                                          <input type="text" value={productName} onChange={(e) => setProductName(e.target.value)} className="pi"
+                                          />
+                                    </div>
+                              </div>
+                              <div class="row">
+                                    <div class="col-25">
+                                          <label className="pn">Product Price</label>
+                                    </div>
+                                    <div class="col-75">
+                                          <input type="text" value={price} onChange={(e) => setPrice(e.target.value)} className="pi" />
+                                    </div>
+                              </div>
+                              <div class="row">
+                                    <div class="col-25">
+                                          <label className="pn">Product Details</label>
+                                    </div>
+                                    <div class="col-75">
+                                          <textarea value={details} onChange={(e) => setDetails(e.target.value)} className="pi" rows="7" cols="50"></textarea>
+                                    </div>
+                              </div>
+                              <div class="row">
+                                    <div class="col-25">
+                                          <label className="pn">Product Description</label>
+                                    </div>
+                                    <div class="col-75">
+                                          <textarea value={description} onChange={(e) => setDescription(e.target.value)} className="pi" rows="7" cols="50"></textarea>
+                                    </div>
+                              </div>
+                              <div class="row">
+                                    <div class="col-25">
+                                          <label className="pn" >Product Image</label>
+                                    </div>
+                                    <div class="col-75">
+                                          <AppDropZone images={images} setImages={setImages} setImageSent={setImageSent} />
+                                          <button className="btn" type="submit" onClick={(e) => {
+                                                e.preventDefault();
+                                                postItem(productName, price, details, description, imageSent, user_id)
+                                                // setFileData(images[0]);
+                                                // console.log(images[0])
+                                                // onSubmitHandler();
+                                          }}>
+                                                Submit
+                                          </button>
+                                    </div>
+                              </div>
+                              <div class="row">
+
+
+                                    {/* <DropZone images={images} setImages={setImages}/> */}
+
+                                    {/* <div className="box__input">
                   <input
                         className="box__file"
                         type="file"
                         onChange={fileChangeHandler}
                   /> */}
 
-            </div>
-            </div>
-      </form>
-      </>
-  )
+                              </div>
+                        </div>
+                  </form>
+            </>
+      )
 }
 
 export default PostItemPage;
